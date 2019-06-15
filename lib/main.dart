b600c9d7e163f5832dab9e98438a0468fc21f7a2
Lucas Andrade <lap.andrade15@gmail.com>
2019-06-15 17:13:07 -0300

    Conclusão

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,50 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _textControler = TextEditingController();
+
   List _toDoList = [];
+
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = new Map();
+      newToDo["title"] = _textControler.text;
+      _textControler.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+      _saveData();
+    });
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return -1;
+        else
+          return 0;
+      });
+    });
+
+    return null;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _readData().then((value) {
+      setState(() {
+        _toDoList = json.decode(value);
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,26 +76,95 @@ class _HomeState extends State<Home> {
                       decoration: InputDecoration(
                           labelText: "Nova tarefa",
                           labelStyle: TextStyle(color: Colors.blueAccent)),
+                      controller: _textControler,
                     ),
                   ),
                   RaisedButton(
                     color: Colors.blueAccent,
                     child: Text("Add"),
                     textColor: Colors.white,
-                    onPressed: () {},
+                    onPressed: _addToDo,
                   )
                 ],
               ),
             ),
+            Expanded(
+                child: RefreshIndicator(
+                    child: ListView.builder(
+                        padding: EdgeInsets.only(top: 10.0),
+                        itemCount: _toDoList.length,
+                        itemBuilder: _itemBuilder),
+                    onRefresh: _refresh))
           ],
         ),
       ),
     );
   }
 
+  Widget _itemBuilder(context, index) {
+    return Dismissible(
+      key: Key(index.toString()),
+      background: Container(
+          color: Colors.red,
+          /* child: Align(
+          alignment: Alignment(0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white),
+        ),*/
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          )),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
+        ),
+        onChanged: (change) {
+          setState(() {
+            _toDoList[index]["ok"] = change;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+          _saveData();
+
+          final snack = SnackBar(
+            duration: Duration(seconds: 2),
+            content: Text("Tarefa ${_lastRemoved["title"]} removida!"),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                }),
+          );
+          Scaffold.of(context).removeCurrentSnackBar();
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+    );
+  }
+
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
-    return File("${directory}/data.json");
+    return File("${directory.path}/data.json");
   }
 
   Future<File> _saveData() async {
